c963c75728d7ec81a89150017b52b3a5e88e64be
SZtamjan <tamjan001@gmail.com>
2024-05-12 14:59:17 +0200

    Patrol State

---
+++ Assets/Scripts/NPCs/NPCAI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace NPCs
+{
+    public class NPCAI : MonoBehaviour
+    {
+        //Components
+        private PatrolArea _patrolArea;
+        private NavMeshAgent _navMeshAgent;
+        
+        //Vars
+        private NPCStates _currentState;
+        
+        //Properties
+        public NPCStates CurrentState => _currentState;
+        public NavMeshAgent NavMeshAgent => _navMeshAgent;
+        
+        private void Start()
+        {
+            PrepareData();
+            ChangeState(NPCStates.patrol);
+        }
+
+        private void PrepareData()
+        {
+            if (TryGetComponent(out PatrolArea patrolArea))
+            {
+                _patrolArea = patrolArea;
+            }
+
+            if (TryGetComponent(out NavMeshAgent navMeshAgent))
+            {
+                _navMeshAgent = navMeshAgent;
+            }
+        }
+
+        private void ChangeState(NPCStates newState)
+        {
+            switch (newState)
+            {
+                case NPCStates.patrol:
+                    _currentState = NPCStates.patrol;
+                    _patrolArea.StartPatrol();
+                    break;
+                case NPCStates.chase:
+                    _currentState = NPCStates.chase;
+                    _patrolArea.StopPatrol();
+                    break;
+                case NPCStates.attack:
+                    _currentState = NPCStates.attack;
+                    _patrolArea.StopPatrol();
+                    break;
+            }
+        }
+        
+    }
+
+    public enum NPCStates
+    {
+        patrol,
+        chase,
+        attack
+    }
+}
---
+++ Assets/Scripts/NPCs/PatrolArea.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NPCs
+{
+    public class PatrolArea : MonoBehaviour
+    {
+        //Components
+        private NPCAI _npcai;
+        
+        //Vars
+        private Coroutine _patrolCoroutine;
+        
+        [SerializeField] private List<Transform> checkPoints;
+        private int _currentIteration = 0;
+        private Transform _currentPoint;
+
+        private void Start()
+        {
+            PrepareData();
+        }
+
+        private void PrepareData()
+        {
+            if (TryGetComponent(out NPCAI npcai))
+            {
+                _npcai = npcai;
+            }
+        }
+
+        public void StartPatrol()
+        {
+            if (_patrolCoroutine == null)
+            {
+                _patrolCoroutine = StartCoroutine(PatrolAreaCor());
+            }
+        }
+        
+        public void StopPatrol()
+        {
+            if (_patrolCoroutine != null)
+            {
+                StopCoroutine(PatrolAreaCor());
+                _patrolCoroutine = null;
+            }
+        }
+
+        private IEnumerator PatrolAreaCor()
+        {
+            while (true)
+            {
+                CheckDestanation();
+                
+                yield return null;
+            }
+        }
+
+        private void CheckDestanation()
+        {
+            if (_currentPoint == null)
+            {
+                _currentIteration = 0;
+                SetDestanation(checkPoints[_currentIteration]);
+                return;
+            }
+
+            if (Vector3.Distance(transform.position, _currentPoint.position) < .1f)
+            {
+                if (++_currentIteration >= checkPoints.Count)
+                {
+                    _currentIteration = 0;
+                }
+                SetDestanation(checkPoints[_currentIteration]);
+            }
+        }
+
+        private void SetDestanation(Transform newDestanation)
+        {
+            _currentPoint = newDestanation;
+            _npcai.NavMeshAgent.destination = _currentPoint.position;
+        }
+    }
+}
